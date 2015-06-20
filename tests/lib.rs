3f3a965066e2b202bcb24b2245416dde0ed70ce2
Camille TJHOA <camille.tjhoa@outlook.com>
2015-06-20 18:55:30 +0200

    Fix test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,12 +1,9 @@
 extern crate fontlib;
 
-use fontlib::FontType;
-
-#[cfg(test)]
-mod test {
-
-    #[test]
-    fn font_type_test() {
-        assert_eq!(fontlib::font_type(), Ok(FontType::TrueType));
+#[test]
+fn font_type_test() {
+    match fontlib::font_type("tests/TrueType/Monaco_Linux.ttf").ok() {
+        Some(fontlib::FontType::TrueType) => (),
+        _ => panic!(),
     }
 }
