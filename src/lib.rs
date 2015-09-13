277cd498a95cca17b5a781f44dcfd61d63af4efd
Camille TJHOA <camille@contract-live.com>
2015-09-13 11:50:10 +0200

    Finish FontType recognizer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(slice_patterns)]
 
 use std::fs::File;
-use std::io::{self, BufReader, Read};
+use std::io::{self, BufReader, Read, Seek, SeekFrom};
 
 pub enum FontType {
     EOT,
@@ -13,13 +13,25 @@ pub enum FontType {
 
 pub fn font_type(source: &str) -> Result<FontType, io::Error> {
     let mut reader = BufReader::new(try!(File::open(source)));
-    let mut buf = [0u8; 4];
+    let mut buf4b = [0u8; 4];
 
-    try!(reader.read(&mut buf));
-    match buf {
-        [0, 1, 0, 0] => Ok(FontType::TrueType),
-        _ => Ok(FontType::WOFF)
+    try!(reader.read(&mut buf4b));
+    match &buf4b {
+        &[0, 1, 0, 0] => Ok(FontType::TrueType),
+        _ => match &buf4b {
+            b"true" | b"typ1" => Ok(FontType::TrueType),
+            b"OTTO" => Ok(FontType::OpenType),
+            b"wOFF" => Ok(FontType::WOFF),
+            _ => {
+                let mut buf2b = [0u8; 2];
+                try!(reader.seek(SeekFrom::Start(34)));
+                try!(reader.read(&mut buf2b));
+                match &buf2b {
+                    b"LP" => Ok(FontType::EOT),
+                    _ => panic!("Font type unknown")
+                }
+            }
+        }
     }
-    
 }
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,43 @@
 extern crate fontlib;
 
+use fontlib::FontType;
+
 #[test]
-fn font_type_test() {
-    match fontlib::font_type("tests/TrueType/Monaco_Linux.ttf").ok() {
-        Some(fontlib::FontType::TrueType) => (),
+fn font_type_ttf1() {
+    match fontlib::font_type("tests/TTF/Monaco_Linux.ttf").ok() {
+        Some(FontType::TrueType) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn font_type_ttf2() {
+    match fontlib::font_type("tests/TTF/ClearSans-Regular.ttf").ok() {
+        Some(FontType::TrueType) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn font_type_otf() {
+    match fontlib::font_type("tests/OTF/Kleymissky_0283.otf").ok() {
+        Some(FontType::OpenType) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn font_type_woff() {
+    match fontlib::font_type("tests/WOFF/ClearSans-Regular.woff").ok() {
+        Some(FontType::WOFF) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn font_type_eot() {
+    match fontlib::font_type("tests/EOT/ClearSans-Regular.eot").ok() {
+        Some(FontType::EOT) => (),
         _ => panic!(),
     }
 }
